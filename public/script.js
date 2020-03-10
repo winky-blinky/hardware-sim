6df4e8e4b0f697184fae9dc6710ecf2cc1bf2ab1
Will Hough <will.hough@nike.com>
2020-03-09 17:09:57 -0700

    Painting the lights now as solid circles when off

--- public/script.js
+++ public/script.js
@@ -1,25 +1,31 @@
 
-var request = new XMLHttpRequest();
+const request = new XMLHttpRequest();
 var winky_blinky;
+const X0 = 20
+const Y0 = 20
 
 function drawLight(context, x, y, color) {
-	const gradient = context.createRadialGradient(x+8, y+8, 2, x+8, y+8, 8);
+	const gradient = context.createRadialGradient(x+X0, y+Y0, 2, x+X0, y+Y0, 8);
 	gradient.addColorStop(0, color);
 	gradient.addColorStop(1, "rgba(0,0,0,0)");
 	context.fillStyle = gradient;
-	context.fillRect(x, y, 16, 16);
-}
-
-function drawRect(context, x, y, color) {
-	console.log("drawing light: ", x, y);
-	context.fillStyle = color;
-	context.fillRect(x, y, 10, 10);
-	context.stroke();
+	context.fillRect(x+X0-5, y+Y0-5, 16, 16);
 }
 
 function paintBackground(ctx, w, h) {
-	ctx.fillStyle = 'black';
-	ctx.fillRect(0, 0, w, h);
+	ctx.fillStyle = '#202020';
+	ctx.fillRect(0, 0, 140, 120);
+}
+
+function drawBoard(context, winky_blinky) {
+	winky_blinky.board.forEach(light => {
+		context.beginPath();
+		context.arc(light.x + X0, light.y + Y0, 5, 0, 2 * Math.PI, false);
+		context.fillStyle = "#303030";
+		context.lineWidth = 1;
+		context.fill();
+		// context.stroke();
+	});
 }
 
 document.addEventListener("DOMContentLoaded", function() {
@@ -38,6 +44,7 @@ document.addEventListener("DOMContentLoaded", function() {
 	request.onload = () => {
 		if(request.status === 200) {
 			winky_blinky = JSON.parse(request.response);
+			drawBoard(context, winky_blinky);
 		}
 	}
 	var socket = io.connect();
