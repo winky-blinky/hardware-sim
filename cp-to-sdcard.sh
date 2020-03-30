5f1b18c1145a39eb5114eab9d092e86682e47e4b
Will Hough <will.hough@nike.com>
2020-03-29 18:57:19 -0700

    Added copy to SD card script

---
+++ cp-to-sdcard.sh
@@ -0,0 +1,4 @@
+cp files/lights-marki.json /Volumes/WINKYBLINKY/l-mi.jso
+cp files/lights-markii.json /Volumes/WINKYBLINKY/l-mii.jso
+cp files/wb-marki.json /Volumes/WINKYBLINKY/wb-mi.jso
+cp files/wb-markii.json /Volumes/WINKYBLINKY/wb-mii.jso
